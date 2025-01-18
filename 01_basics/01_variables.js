e9558b3f784038f07672b9e7723c7788665fff96
Daveyyy <23it012@charusat.edu.in>
2025-01-18 13:20:45 +0000

    variables and datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accId = 9852
+let mail = "dev@gmail.com"
+var pass = "123"
+accCity = "Baroda"
+
+//accId = 12
+mail = "DEV@hm.com"
+pass = "5050"
+accCity = "Nashik"
+
+console.log(accId);
+console.table([mail, pass, accCity]);
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,10 @@
+// number => 2 to the power of 53
+// bigint => very very big
+// string => '' or ""
+// boolean => true or false
+// null => standalone value 
+// undefined
+// symbol => to find uniqueness ig
+
+// object =>
+// typeof => to know the type of any container (null ka object aayega) (undefined ka undefined)
